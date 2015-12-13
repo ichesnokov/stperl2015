d8aed494a9323ef19ccfd4f61193cb6ce7181359
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2015-12-14 02:06:00 +0300

    Comments

--- anyevent/watchers.pl
+++ anyevent/watchers.pl
@@ -1,22 +1,29 @@
 #!/usr/bin/env perl
 use common::sense;
+use utf8::all;
 use AE;
 
+# Prepace condvar
 my $cv = AE::cv;
+
+# Set up I/O watcher
 my $io_w = AE::io *STDIN, 0, sub {
     chomp(my $typed = <STDIN>);
-    utf8::decode($typed);
     say "Вы ввели: $typed";
     $cv->send('Good-bye') if $typed eq 'exit';
 };
 
 my $counter = 0;
+
+# Set up idle watcher
 my $idle_w = AE::idle sub {
     say "Я досчитал до $counter!" if ++$counter % 5e6 == 0;
 };
 
+# Set up timer watcher
 my $timer_w = AE::timer 5, 5, sub {
     say "Прошло еще 5 секунд";
 };
 
+# Block until $cv receives something
 say $cv->recv;
