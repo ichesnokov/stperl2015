3d5a556506f71be50dc32466588409b31ee82c65
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2015-12-13 23:00:20 +0300

    Perl examples

---
+++ forks-many.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use v5.18;
+use warnings;
+use forks;
+
+my $COUNT       = 1e3;
+my $MAX_THREADS = 5000;
+my @threads;
+for my $i (1 .. $MAX_THREADS) {
+    push @threads, threads->create(
+        sub {
+            # some asynchronous thread of execution
+            my $result = 0;
+            for my $i (1 .. $COUNT) {
+                $result += $i;
+            }
+            say "Thread $i result: $result";
+        }
+    );
+}
+$_->join for @threads;
+say 'Done';
