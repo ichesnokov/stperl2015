a7ec082aebaa57b2655b881483ebfc6b225bbfb4
Ilya Chesnokov <me@ichesnokov.ru>
2015-12-18 22:52:36 +0300

    s/AE/AnyEvent/

--- examples/anyevent/watchers.pl
+++ examples/anyevent/watchers.pl
@@ -1,29 +1,35 @@
 #!/usr/bin/env perl
 use common::sense;
 use utf8::all;
-use AE;
+use AnyEvent;
 
-# Prepace condvar
-my $cv = AE::cv;
+my $cv = AnyEvent->condvar;
 
 # Set up I/O watcher
-my $io_w = AE::io *STDIN, 0, sub {
-    chomp(my $typed = <STDIN>);
-    say "Вы ввели: $typed";
-    $cv->send('До свиданья!') if $typed eq 'exit';
-};
-
-my $counter = 0;
+my $io_w = AnyEvent->io(
+    fh   => *STDIN,
+    poll => 'r',
+    cb   => sub {
+        chomp(my $typed = <STDIN>);
+        say "Вы ввели: $typed";
+        $cv->send('До свиданья!') if $typed eq 'exit';
+    }
+);
 
 # Set up idle watcher
-my $idle_w = AE::idle sub {
+my $counter = 0;
+my $idle_w = AnyEvent->idle(sub {
     say "Я досчитал до $counter!" if ++$counter % 5e6 == 0;
-};
+});
 
 # Set up timer watcher
-my $timer_w = AE::timer 5, 5, sub {
-    say "Прошло еще 5 секунд";
-};
+my $timer_w = AnyEvent->timer(
+    after    => 5,
+    interval => 5,
+    cb       => sub {
+        say "Прошло еще 5 секунд";
+    }
+);
 
 # Block until $cv receives something
 say $cv->recv;
