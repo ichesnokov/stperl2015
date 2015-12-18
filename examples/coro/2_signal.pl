2d17d56553356237ba5580fb914ef074167f51ee
Ilya Chesnokov <me@ichesnokov.ru>
2015-12-19 01:00:37 +0300

    more compact code

--- examples/coro/2_signal.pl
+++ examples/coro/2_signal.pl
@@ -4,25 +4,19 @@ use Coro;
 use Coro::AnyEvent;
 
 my $sig = Coro::Signal->new;
-async {
-    say "First thread starts waiting...";
-    $sig->wait;
-    say "First thread woke up!";
-};
-
-async {
-    say "Second thread starts waiting...";
-    $sig->wait;
-    say "Second thread woke up!";
-};
+for my $name (qw(First Second)) {
+    async {
+        say "$name thread starts waiting...";
+        $sig->wait;
+        say "$name thread woke up!";
+    };
+}
 
 async {
     say "Sleeping 1 second...";
-
     # Blocking sleep
     sleep 1;
     $sig->send;
 };
-
 
 Coro::AnyEvent::idle;
