ef65c976486a00e925b03fb03ea9063c8b62711e
Ilya Chesnokov <me@ichesnokov.ru>
2015-12-19 00:58:49 +0300

    We're not pretending

--- examples/coro/1_semaphore.pl
+++ examples/coro/1_semaphore.pl
@@ -7,13 +7,10 @@ my $sem = Coro::Semaphore->new(5);
 for my $i (1 .. 1000) {
     async {
         $sem->down;
-
         say "I am coro number $i";
 
-        # Pretend we're doing something asynchronously:
-        # use non-blocking sleep
+        # Use non-blocking sleep
         Coro::AnyEvent::sleep 1;
-
         $sem->up;
     };
 }
