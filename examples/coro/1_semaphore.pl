d431c7b08405ad0c21ea2ef4dba9fba4f41df958
Ilya Chesnokov <me@ichesnokov.ru>
2015-12-18 03:02:55 +0300

    fix semaphore example

--- examples/coro/1_semaphore.pl
+++ examples/coro/1_semaphore.pl
@@ -4,19 +4,17 @@ use Coro;
 use Coro::AnyEvent;
 
 my $sem = Coro::Semaphore->new(5);
-
-my $iter = 0;
 for my $i (1 .. 1000) {
     async {
         $sem->down;
 
         say "I am coro number $i";
 
-        # Pretend we're doing something asynchronously using non-blocking sleep
+        # Pretend we're doing something asynchronously:
+        # use non-blocking sleep
         Coro::AnyEvent::sleep 1;
 
         $sem->up;
     };
 }
-
 Coro::AnyEvent::sleep 2;
